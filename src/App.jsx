7ca21af5686cc2004b70eab3695d3fa2a6d0ccc5
mdsameeralam9 <mdsameer.alam9@gmail.com>
2025-08-27 21:26:17 +0530

    remove extra code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-
-import { QueryClient, QueryClientProvider } from './api_call/index';
-import ProductListWrapper from './ProductListWrapper';
+import { QueryClient, QueryClientProvider } from "./api_call/index";
+import ProductListWrapper from "./ProductListWrapper";
 
 function App() {
-
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
-          <ProductListWrapper />
-        </QueryClientProvider>
-  )
+      <ProductListWrapper />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
